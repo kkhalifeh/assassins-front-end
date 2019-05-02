eb1c414ec8447d543ad7333ae36d36d5a6574c20
Khaled Khalifeh <khaled.khalifeh@gmail.com>
2019-05-02 15:55:24 -0400

    completed user sign up form

---
+++ src/components/StartGame.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+const API = 'http://localhost:3000/games/start/'
+
+class StartGame extends Component {
+
+  onSubmit = (e) => {
+    const game = { id: 1 }
+    e.preventDefault()
+    fetch(API, {
+      method: 'POST', // or 'PUT'
+      body: JSON.stringify(game), // data can be `string` or {object}!
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(res => res.json())
+      .then(response => console.log('Success:', JSON.stringify(response)))
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.onSubmit}>
+        <div className="form-row">
+          <input
+            type="submit"
+            value="Start Game"
+            className="btn btn-dark btn-block" />
+        </div>
+      </form>
+    )
+  }
+}
+
+export default StartGame
---
+++ src/components/UserSignup.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react'
+const API = 'http://localhost:3000/users/create/'
+
+class UserSignup extends Component {
+
+  state = {
+    name: '',
+    alias: '',
+    password_digest: '',
+    id: 1
+  }
+
+  onChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  onSubmit = (e) => {
+    const user = { ...this.state }
+    if (user.name !== '' && user.alias !== '' && user.password !== '') {
+      console.log(user);
+      e.preventDefault()
+      fetch(API, {
+        method: 'POST', // or 'PUT'
+        body: JSON.stringify(user), // data can be `string` or {object}!
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).then(res => res.json())
+        .then(response => console.log('Success:', JSON.stringify(response)))
+    }
+  }
+
+
+
+  render() {
+    const { name, alias, game, password_digest } = this.state
+    return (
+      <form onSubmit={this.onSubmit}>
+        <div className="form-row">
+          <div className="form-group col-md-6">
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              className="form-control"
+              name="name"
+              placeholder="Enter name"
+              value={name}
+              onChange={this.onChange} />
+          </div>
+          <div className="form-group col-md-6">
+            <label htmlFor="name">Alias</label>
+            <input
+              type="text"
+              className="form-control"
+              name="alias"
+              placeholder="Enter name"
+              value={alias}
+              onChange={this.onChange} />
+          </div>
+          <div className="form-group col-md-6">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              name="password_digest"
+              placeholder="password"
+              value={password_digest}
+              onChange={this.onChange} />
+          </div>
+          <input
+            type="submit"
+            value="Sign Up"
+            className="btn btn-dark btn-block" />
+        </div>
+      </form>
+    )
+  }
+}
+
+export default UserSignup
